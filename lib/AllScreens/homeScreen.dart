a5f9a42d583ad9965c0bdd674121609b6ec20255
GDaniil17 <32881349+GDaniil17@users.noreply.github.com>
2021-04-25 14:34:41 +0300

    4

--- lib/AllScreens/homeScreen.dart
+++ lib/AllScreens/homeScreen.dart
@@ -1,4 +1,4 @@
-import 'dart:html';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_app/AllScreens/mainscreen.dart';
@@ -8,152 +8,187 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String idScreen = "home";
+  File _image;
+
+
+  Future getImage() async {
+    final picker = ImagePicker();
+      final pickedFile = await picker.getImage(source: ImageSource.gallery);
+      if (pickedFile != null){
+        Fluttertoast.showToast(msg: "Ура!");
+      }else{
+        Fluttertoast.showToast(msg: "Плак!");
+      }
+/*
+      this..setState(() {
+        if (pickedFile != null) {
+          _image = File(pickedFile.path);
+        } else {
+          print('Ничего не выбранно');
+        }
+      });
+
+ */
+    }
+
+  Future takePicture() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+    if (pickedFile != null){
+      Fluttertoast.showToast(msg: "Ура!");
+    }else{
+      Fluttertoast.showToast(msg: "Плак!");
+    }
+    /*
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('Снимка нет.');
+      }
+    });
+
+     */
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Center(
-        child: SizedBox(
-        width: 500,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
 
-              Text(
-                'Форма отправки',
-                style: TextStyle(fontSize: 50.0, fontStyle:FontStyle.italic),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  fillColor: Colors.cyan,
-                  border: OutlineInputBorder(),
-                  labelText: 'Объект',
-                  contentPadding: EdgeInsets.all(20.0),
-
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Форма отправки',
+                  style: TextStyle(fontSize: 50.0, fontStyle: FontStyle.italic),
                 ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Владелец',
-                  contentPadding: EdgeInsets.all(20.0),
-
+                SizedBox(
+                  height: 5,
                 ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 100,
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Причина инцидента',
-                  contentPadding: EdgeInsets.all(20.0),
+                TextField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.cyan,
+                    border: OutlineInputBorder(),
+                    labelText: 'Объект',
+                    contentPadding: EdgeInsets.all(20.0),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 100,
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Продукт',
-                  contentPadding: EdgeInsets.all(20.0),
+                SizedBox(
+                  height: 20.0,
                 ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 100,
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Пострадавшие объекты',
-                  contentPadding: EdgeInsets.all(20.0),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Владелец',
+                    contentPadding: EdgeInsets.all(20.0),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 100,
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Ведутся работы по устранению',
-                  contentPadding: EdgeInsets.all(20.0),
+                SizedBox(
+                  height: 20.0,
+                  width: 100,
                 ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 100,
-              ),
-              FloatingActionButton.extended(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.image,
-                  size: 40,
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Причина инцидента',
+                    contentPadding: EdgeInsets.all(20.0),
+                  ),
                 ),
-                label: Text(
-                  "Загрузить",
-                  style: TextStyle(fontSize: 40.0),
+                SizedBox(
+                  height: 20.0,
+                  width: 100,
                 ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 100,
-              ),
-              FloatingActionButton.extended(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.add_a_photo,
-                  size: 40,
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Продукт',
+                    contentPadding: EdgeInsets.all(20.0),
+                  ),
                 ),
-                label: Text(
-                  "Сделать фото",
-                  style: TextStyle(fontSize: 40.0),
+                SizedBox(
+                  height: 20.0,
+                  width: 100,
                 ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 100,
-              ),
-              FloatingActionButton.extended(
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Пострадавшие объекты',
+                    contentPadding: EdgeInsets.all(20.0),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 100,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Ведутся работы по устранению',
+                    contentPadding: EdgeInsets.all(20.0),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 100,
+                ),
+                FloatingActionButton.extended(
                   onPressed: () {
-                    Navigator.pushNamedAndRemoveUntil(
-                        context, MainScreen.idScreen, (route) => false);
-
+                    getImage();
                   },
                   icon: Icon(
-                    Icons.send,
+                    Icons.image,
                     size: 40,
                   ),
                   label: Text(
-                    "Отправить",
+                    "Загрузить",
                     style: TextStyle(fontSize: 40.0),
-                  )),
-            ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 100,
+                ),
+                FloatingActionButton.extended(
+                  onPressed: () {
+                    takePicture();
+                  },
+                  icon: Icon(
+                    Icons.add_a_photo,
+                    size: 40,
+                  ),
+                  label: Text(
+                    "Сделать фото",
+                    style: TextStyle(fontSize: 40.0),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 100,
+                ),
+                FloatingActionButton.extended(
+                    onPressed: () {
+                      Navigator.pushNamedAndRemoveUntil(
+                          context, MainScreen.idScreen, (route) => false);
+                    },
+                    icon: Icon(
+                      Icons.send,
+                      size: 40,
+                    ),
+                    label: Text(
+                      "Отправить",
+                      style: TextStyle(fontSize: 40.0),
+                    )),
+              ],
+            ),
           ),
-        ),
-      ),
-      ),
+        )
     );
   }
 }
